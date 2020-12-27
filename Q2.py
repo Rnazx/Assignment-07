3849a9bbe77f112dc88c17aea984ffec867dc219
Rnazx <42196798+Rnazx@users.noreply.github.com>
2020-12-27 11:43:22 +0530

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -10,5 +10,5 @@ diff.rungekutta4(0,2,1,g,f,0.02,-5,5)
 diff.rungekutta4(0,2,1,g,f,0.07,-5,5)
 #output
 #This code generates a data file. The datapoints are plotted along with the analytical function which is 1+exp(-x)-(x^2)/2+2*x
-#The data file is named "RK4*insert value of h*.txt" and the plot is named  "Q2_Rk4_h=*insert value of h*.pdf"
+#The data file is named "RK4*insert value of h*.txt" and the plot is named  "Q2_Rk4_h=*insert value of h*.pdf".
 
